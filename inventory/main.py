ad7e7aee873b131cfbed067b7b29f3baeafb8ef1
Narayan soni <narayansoni854@gmail.com>
2023-12-06 03:23:05 +0530

    .env configuration and redis connection setup

--- inventory/main.py
+++ inventory/main.py
@@ -1,15 +1,15 @@
 from typing import Union
-
+from redis_om import get_redis_connection, HashModel
 from fastapi import FastAPI
+import os
+from config import settings
+
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+redis = get_redis_connection(
+    host=settings.host,
+    port=settings.port,
+    password=settings.password,
+    decode_responses=True,
+)
