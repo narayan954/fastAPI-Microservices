38cff618e57d344dc81d8d651286ce8e6b30d9cb
Narayan soni <narayansoni854@gmail.com>
2023-12-07 02:10:06 +0530

    add cors

--- inventory/main.py
+++ inventory/main.py
@@ -1,11 +1,17 @@
-from typing import Union
 from redis_om import get_redis_connection, HashModel
 from fastapi import FastAPI
-import os
 from config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 redis = get_redis_connection(
     host=settings.host,
